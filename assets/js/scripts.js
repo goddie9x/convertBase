5ceca6ff0637a097ebe10fdb013c2b90d48d1710
goddie9x <goddie9x@yahoo.com>
2021-07-21 21:51:20 +0700

    big update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,47 +44,12 @@ function setCanvasParticle(position = 'relative', zIndex = 20) {
 
 setCanvasParticle('fixed', 3);
 
-function convs(num, typeBase = 2) {
-    let number;
-    if (num == 0) {
-        return 0;
-    } else {
-        number = num % typeBase;
-    }
-    return convs(Math.floor(num / typeBase), typeBase) * 10 + number;
-}
-
-function myTrimFZero(x) {
-
-    if (typeof(x) == 'string') {
-        return x.replace(/^0+/, '');
-    }
-    if (Array.isArray(x)) {
-        return x.join("").trim().replace(/^0+/, '');
-    }
-}
-
-function reVertToDecimal(num, typeBase = 2) {
-    let rawNum = [...num.toString().trim()];
-    let isNegative = 0;
-
-    if (rawNum[0] == '-') {
-        isNegative = typeBase - 1;
-        rawNum.shift();
+function decimalToHexString(number) {
+    if (number < 0) {
+        number = 0xFFFFFFFF + number + 1;
     }
 
-    let textNum = myTrimFZero(rawNum);
-
-    let result = 0;
-    let length = textNum.length;
-
-    for (let i = length - 1; i >= 0; i--) {
-        result += Math.pow(typeBase, i) * textNum[length - 1 - i];
-    }
-    if (isNegative) {
-        result = [isNegative, [...[...result.toString()].reverse()]].join();
-    }
-    return result;
+    return number.toString(16).toUpperCase();
 }
 
 function base64(text, type = 'en') {
@@ -120,19 +85,27 @@ function convertBase(formID) {
 
     inputs.forEach(function(input, index) {
         input.oninput = function() {
+            inputs.forEach(element => {
+                $(element).next().html('');
+            });
             let currentBase = input.getAttribute('base');
-            let valueInputToDecimal = reVertToDecimal(input.value, currentBase);
+            let valueInputToDecimal = parseInt(input.value, currentBase);
 
-            if (Number.isInteger(input.value) && currentBase != 16) {
-                console.error('nhập chữ');
+            if (isNaN(input.value) && currentBase != 16) {
+                $(input).next().html('Vui lòng nhập số dương');
             } else {
                 inputs.forEach(function(input2, index2) {
                     if (index2 != index) {
                         let typeBase = input2.getAttribute('base');
-                        if (typeBase == 16) {
 
+                        if (isNaN(valueInputToDecimal)) {
+                            $(input).next().html('Vui lòng nhập đúng kiểu dữ liệu');
                         } else {
-                            input2.value = convs(valueInputToDecimal, typeBase);
+
+                            if (typeBase == 16) {
+                                input2.value = decimalToHexString(valueInputToDecimal);
+                            }
+                            input2.value = valueInputToDecimal.toString(typeBase);
                         }
                     }
                 })
